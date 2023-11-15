d39571c98300a9f65b8d57a4871f79eef296e9fb
BrianSzeto <144480756+BrianSzeto@users.noreply.github.com>
2023-11-15 10:24:33 -0500

    added new creation thing
    asdfklj adjf laksdjf klasj fklas dfkjasdkl fj aslkdfj asl;kdfj sdl;jf lkadjfl; k

--- cardMaker.py
+++ cardMaker.py
@@ -1,51 +1,50 @@
 import json
 import os
-more = "True"
 ## Create Class for creating new dictionaries here
-class card:
-    def makeCard(self,name,hp,ability,specialMana):
+
+nameinput = input("Name: ")
+gradeinput = input("Grade: ")
+classinput = input("Class: ")
+teacherinput = input("Teacher: ")
+periodinput = input("Period: ")
+averageinput = input("Class Average: ")
+print("Thank you for your information, it is now publicly displayed in data.json")
+class student:
+    def __init__(self, name, grade, class1, teacher, period, average):
         self.name = name
-        self.hp = hp
-        self.ability = ability
-        self.specialMana = specialMana
-        card = {'name':name,'hp':hp,'special ability':ability,'special ability cost':specialMana}
-        return card
+        self.grade = grade
+        self.class1 = class1
+        self.teacher = teacher
+        self.period = period
+        self.average = average
+    def create(self):
+        data.append({'name': self.name, 'grade': self.grade, 'class': self.class1, 'teacher': self.teacher, 'period': self.period, 'class average': self.average})
 
-again = "true"
-while again == "true":
-    with open("data.json", "r") as f:
-        # Serialize the updated Python list to a JSON string
-        data = json.load(f)
-        ##Call classes in here
-        card = card()
-        name = input("State the card's name: ")
-        hp = int(input("State the card's HP: "))
-        ability = input("State the card's ability name: ")
-        specialMana = int(input("State the card's ability cost: "))
-        newCard = card.makeCard(name, hp, ability, specialMana)
-        data.append(newCard)
-        print(data)
+
+
+with open("data.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+    ##Call classes in here
+    
+    run = student(f"{nameinput}", f"{gradeinput}", f"{classinput}", f"{teacherinput}", f"{periodinput}", f"{averageinput}")
+    run.create()
 
 
 
 
 
 
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data, indent=4)
 
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
 
-    #No code needed below this line
-    # Creates a new JSON file with the updated data
-    new_file = "updated.json"
-    with open(new_file, "w") as f:
-        # Serialize the updated Python list to a JSON string
-        json_string = json.dumps(data, indent= -4)
-
-        # Write the JSON string to the new JSON file
-        f.write(json_string)
-
-    # Overwrite the old JSON file with the new one
-    os.remove("data.json")
-    os.rename(new_file, "data.json")
-    userMore = input("Do you want to add another card (Y/N): ")
-    if userMore == "N":
-        again = "false"
+# Overwrite the old JSON file with the new one
+os.remove("data.json")
+os.rename(new_file, "data.json")
