8d46e29342ed5bed0ae061457f88470d29d464a2
Laithanator <144480666+Laithanator@users.noreply.github.com>
2023-11-20 14:02:55 -0500

    edhdtthfht

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 import json
+import random
+
 test = open("data.json", encoding="utf8")
 data = json.load(test)
-print(data)
-print(len(data))
+
+
+player1 = []
+player2 = []
+length = len(data)
+
+
+def player1turn():
+    for i in range(len(player1)):
+        print(player1[i], end ='\n')
+    cardUse = input("State the name of the card you'd like to use: ")
+    special = input("State whether you'd like to use your card's special ability (Y/N): ")
+    cardInteract = (input("State the name of the card you'd like to interact with: "))
+    for i in range(length):
+        print(data[i])
+        if data[i]["name"] == data[i][cardUse]:
+            cardUseID = i
+        if data[i]["name"] == data[i][cardInteract]:
+            cardInteractID = i
+    if special == 'Y':
+        specialAttackDmg = data[i]['special ability damage']
+        print(specialAttackDmg)
+player1turn()
+
--- test2.py
+++ test2.py
@@ -4,12 +4,7 @@ test = open("data.json", encoding="utf8")
 data = json.load(test)
 player1 = []
 player2 = []
-
-for i in range(5):
-    card = random.randint(0,len(data)-1)
-    player1.append(data[card])
-for i in range(5):
-    card = random.randint(0,len(data)-1)
-    player2.append(data[card])
-
-print('Player 1 Cards: ',player1,'Player 2 Cards: ',player2)
+cardUse = str(input("test: "))
+print(data[1][cardUse])
+if data[1]["name"] == data[1][cardUse]:
+    print("good")
