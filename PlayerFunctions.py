1d292c781429be7e56e908a143531420f307bd8b
Laithanator <144480666+Laithanator@users.noreply.github.com>
2023-11-22 13:05:45 -0500

    efsefseffe

--- PlayerFunctions.py
+++ PlayerFunctions.py
@@ -32,41 +32,102 @@ cardAttackID = 1
 newCardListID = 1
 firstPlayer = random.randint(1,2)
 playerCards = 'Alive'
-nextListPart = 0
 player1HP = []
 player2HP = []
 
-for i in range(5):
-    for i in range(length):
-        if player1[nextListPart]['name'] in data[i]['name']:
-            player1HP.append(data[i]['health'])
-            nextListPart += 1
-print(player1HP)
+
+
+for i in range(length):
+    if player1[0]['name'] in data[i]['name']:
+        player1HP.append(data[i]['hp'])
+for i in range(length):
+    if player1[1]['name'] in data[i]['name']:
+        player1HP.append(data[i]['hp'])
+for i in range(length):
+    if player1[2]['name'] in data[i]['name']:
+        player1HP.append(data[i]['hp'])
+for i in range(length):
+    if player1[3]['name'] in data[i]['name']:
+        player1HP.append(data[i]['hp'])
+for i in range(length):
+    if player1[4]['name'] in data[i]['name']:
+        player1HP.append(data[i]['hp'])
+
+for i in range(length):
+    if player2[0]['name'] in data[i]['name']:
+        player2HP.append(data[i]['hp'])
+for i in range(length):
+    if player2[1]['name'] in data[i]['name']:
+        player2HP.append(data[i]['hp'])
+for i in range(length):
+    if player2[2]['name'] in data[i]['name']:
+        player2HP.append(data[i]['hp'])
+for i in range(length):
+    if player2[3]['name'] in data[i]['name']:
+        player2HP.append(data[i]['hp'])
+for i in range(length):
+    if player2[4]['name'] in data[i]['name']:
+        player2HP.append(data[i]['hp'])
 
 
 
-def player1turn():
-    for i in range(length):
-        print(player1[i], end ='\n')
-    cardUse = input("State the name of the card you'd like to use: ")
-    special = int(input("State whether you'd like to use your card (Y/N): "))
-    cardInteract = (input("State the name of the card you'd like to interact with: "))
-    for i in range(length):
-        if cardUse in data[i]['name']:
-            cardUseID = i
-        if cardUse in data[i]['name']:
-            cardAttackID = i
 
-    for i in range(len(player2)):
-        if cardInteract in player2[i]:
-            newCardListID = i
-    
-    if special == 'Y':
-        specialAttackDmg = data[cardUseID]['special ability damage']
+class playerTurns():
+    def player1turn(self):
+        print("Player 1's turn!\n")
+        cardUse = input("State the name of the card you'd like to use: ")
+        special = (input("State whether you'd like to use your card's special ability (Y/N): "))
+        cardInteract = (input("State the name of the card you'd like to interact with: "))
+        for i in range(length):
+            if cardUse in data[i]['name']:
+                cardUseID = i
+            if cardUse in data[i]['name']:
+                cardAttackID = i
 
-        
+        for i in range(len(player2)):
+            if cardInteract in player2[i]:
+                interactCardListID = i
+
+        if special == 'Y':
+            specialAttackDmg = data[cardUseID]['special ability damage']
+            interactCardHP = interactCardListID - specialAttackDmg
+            player2[interactCardListID]['hp'] = interactCardHP
+        else:
+            normalAttackDmg = player1[cardUseID]['damage']
+            interactCardHP = interactCardListID - normalAttackDmg
+            player2[interactCardListID]['hp'] = interactCardHP
+
+    def player2turn(self):
+        print("Player 2's turn!\n")
+        cardUse = input("State the name of the card you'd like to use: ")
+        special = (input("State whether you'd like to use your card's special ability (Y/N): "))
+        cardInteract = (input("State the name of the card you'd like to interact with: "))
+        for i in range(length):
+            if cardUse in data[i]['name']:
+                cardUseID = i
+            if cardUse in data[i]['name']:
+                cardAttackID = i
+
+        for i in range(len(player1)):
+            if cardInteract in player1[i]:
+                interactCardListID = i
+
+        if special == 'Y':
+            specialAttackDmg = data[cardUseID]['special ability damage']
+            interactCardHP = interactCardListID - specialAttackDmg
+            player1[interactCardListID]['hp'] = interactCardHP
+        else:
+            normalAttackDmg = player2[cardUseID]['damage']
+            interactCardHP = interactCardListID - normalAttackDmg
+            player1[interactCardListID]['hp'] = interactCardHP
+
+playerturn = playerTurns()   
 
 
 while playerCards == 'Alive':
     if firstPlayer == 1:
-        print('Hi')
+        playerturn.player1turn()
+        playerturn.player2turn()
+    else:
+        playerturn.player2turn()
+        playerturn.player1turn()
