acf8dccc529eb722283474b996400ca78fc89760
BrianSzeto <brianszeto2@gmail.com>
2023-12-09 17:19:38 -0500

    progress
    don't worry

--- brainmain.py
+++ brainmain.py
@@ -4,4 +4,7 @@ test = open("data.json", encoding="utf8")
 data = json.load(test)
 
 game = Game()
-game.normal_attack()
+game.choosecard()
+special_ability = input("Would you like to use your special ability? Y/N: ").upper()
+if special_ability == "Y":
+    game.special()
--- functions.py
+++ functions.py
@@ -3,7 +3,8 @@ class Game():
     def __init__(self):
         import random
         import json
-        remaining_cards = 5
+        remaining_cards1 = 5
+        remaining_cards2 = 5
         test = open("data.json", encoding="utf8")
         data = json.load(test)
         player1cards = []
@@ -32,14 +33,66 @@ class Game():
         self.player1cards = player1cards
         self.player2cards = player2cards
         self.playerturn = playerturn
-        self.remaining_cards = remaining_cards
-        print(player1cards)
+        self.remaining_cards1 = remaining_cards1
+        self.remaining_cards2 = remaining_cards2
     @classmethod
-    def normal_attack(self):
-        print(self.player1cards)
-        x = 0
+    def choosecard(self):
         if self.playerturn == 0:
+            used_card = []
+            good = 0
             print("Your cards are: \n")
-            while x < 6:
-                print(self.player1cards[x]"\n")
-                x += 1
+            for i in range(self.remaining_cards1):
+                print(self.player1cards[i],"\n")
+            #typo check
+            while good != 1:
+                use = input("What card would you like to use?: ")
+                for i in range(self.remaining_cards1):
+                    if use in self.player1cards[i]['name']:
+                        used_card.extend([self.player1cards[i]])
+                if not used_card:
+                    print("No results found\nTry again")
+                else:
+                    good = 1
+            self.used_card = used_card
+        if self.playerturn == 1:
+            used_card = []
+            good = 0
+            print("Your cards are: \n")
+            for i in range(self.remaining_cards2):
+                print(self.player2cards[i],"\n")
+            #typo check
+            while good != 1:
+                use = input("What card would you like to use?: ")
+                for i in range(self.remaining_cards2):
+                    if use in self.player2cards[i]['name']:
+                        used_card.extend([self.player2cards[i]])
+                if not used_card:
+                    print("No results found\nTry again")
+                else:
+                    good = 1
+            self.used_card = used_card
+    def special(self):
+        if self.used_card[0]['ability type'] == 'attack':
+            good = 0
+            attacked_card = []
+            while good != 1:
+                if self.playerturn == 0:
+                    attack_card = input("What card would you like to interact with?: ")
+                    for i in range(self.remaining_cards2):
+                        if attack_card in self.player2cards[i]['name']:
+                            attacked_card.extend([self.player2cards[i]])
+                    if not attacked_card:
+                        print("No results found\nTry again")
+                    else:
+                        good = 1
+                if self.playerturn == 1:
+                    attack_card = input("What card would you like to interact with?: ")
+                    for i in range(self.remaining_cards1):
+                        if attack_card in self.player1cards[i]['name']:
+                            attacked_card.extend([self.player1cards[i]])
+                    if not attacked_card:
+                        print("No results found\nTry again")
+                    else:
+                        good = 1
+            attacked_card[0]['hp'] = attacked_card[0]['hp']-self.used_card[0]['special ability damage']
+            print(attacked_card[0]['hp'])
